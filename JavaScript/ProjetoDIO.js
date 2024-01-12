89da1cd531fd642aed8272da2788bfcc5f89191f
Irinaldo Santos <irinaldo01santos@gmail.com>
2024-01-12 20:14:05 -0300

    Desafio Dio

--- JavaScript/ProjetoDIO.js
+++ JavaScript/ProjetoDIO.js
@@ -1,1 +1,29 @@
-console.log("Olá Mundo")
+let nome = "Jhin"
+let XP = 6500
+
+if(XP < 1000){ 
+    XP = "Ferro"
+
+}else if(XP >= 1001 && XP <= 2000){
+    XP = "Bronze"
+
+}else if(XP >=2001 && XP <= 5000){
+    XP = "Prata"
+
+}else if(XP >=6001 && XP <= 7000){
+    XP = "Ouro"
+
+}else if(XP >=70001 && XP <= 8000){
+    XP = "Platina"
+
+}else if(XP >=8001 && XP <= 9000){
+    XP = "Ascendente"
+
+}else if(XP >=9001 && XP <= 10000){
+    XP = "Imortal"
+
+}else if (XP >= 10001){
+    XP = "Radiante"
+
+}
+console.log("O heroi de nome "+ nome + " está no nível de " + XP)
